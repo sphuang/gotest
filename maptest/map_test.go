f5f92a676339e18347a1d84cd6a3e7582cdb63c4
Shinbo Huang <shinbo@maicoin.com>
2021-01-21 16:48:40 +0800

    maptest - Add init test

--- maptest/map_test.go
+++ maptest/map_test.go
@@ -6,6 +6,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestInit(t *testing.T) {
+	var m map[int]string
+	assert.Nil(t, m)
+
+	// nil map can't insert element
+	// m[0] = "zero"
+	// assert.Equal(t, "zero", m[0])
+
+	m2 := map[int]string{
+		1: "one",
+		2: "two",
+	}
+	m2[0] = "zero"
+	assert.Equal(t, "zero", m2[0])
+
+	m3 := make(map[int]string)
+	m3[0] = "zero"
+	assert.Equal(t, "zero", m3[0])
+}
+
 func TestAccess(t *testing.T) {
 	m := map[int]int{1: 100, 2: 200, 3: 300}
 
